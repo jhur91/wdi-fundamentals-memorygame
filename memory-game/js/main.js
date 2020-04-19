7c88d0a51cff072a0967afc60bd2b34fbfd3c03f
Jason Hur <jasonhur@Jasons-MacBook-Pro.local>
2020-04-18 18:57:21 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -29,22 +29,37 @@ function checkForMatch() {
   alert("You found a match!");
 } else {
   alert("Sorry, try again.");
-}
-}
+};
+};
 
-function flipCard(cardId) {
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	//console.log("User flipped " + cards[cardId].rank);
+	//console.log(cards[cardId].cardImage);
+	//console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
-		}
+		};
 	
+};
+
+
+function createBoard() {
+	for(let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
+
+
+
 
 
 
